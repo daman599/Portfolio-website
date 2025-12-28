6a347ff7f03c1d28c4d75220a4f96898307b73f4
daman599 <damanpreetk117@gmail.com>
2025-12-28 16:37:37 +0530

    new resume added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -28,7 +28,7 @@ export default function About() {
                 </Link>
 
                 <a
-                    href="https://drive.google.com/file/d/1E5lM5kl3v_gr8WANsV0AEwwCmpv0bZ3S/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1ejJ-klXoLvlB9J9ZhhDICw1-c3wWDjCm/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
